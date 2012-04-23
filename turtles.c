a079a7c46f9baf8acd8740c74b94114324e80700
Urs Thuermann <urs@isnogud.escape.de>
2012-04-23 16:03:21 +0200

    Simplify code
    * Remove the card number from struct CARD.
    * Replace arrays CARD c[] and int r[] with an array of structs.
    * Don't copy card to c[] and don't rotate them in c[], but instead
      store the card's number and rotation in an array of structs.
    * Provide a function mark() that returns the mark on a placed card at
      a given edge.

--- turtles.c
+++ turtles.c
@@ -1,5 +1,5 @@
 /*
- * $Id: turtles.c,v 1.9 2012/04/23 14:03:11 urs Exp $
+ * $Id: turtles.c,v 1.10 2012/04/23 14:03:21 urs Exp $
  *
  * Solve the turtle game.
  */
@@ -7,29 +7,31 @@
 #include <stdio.h>
 
 static void place_card(int idx);
-static void rotate(int idx);
 static int  check(int idx);
+static int  mark(int idx, int dir);
 static void print(int idx, int brk_flag);
 
 typedef struct {
-	int no;
 	int mark[4];
 } CARD;
 
+enum { RIGHT, UP, LEFT, DOWN };
+
 static const CARD card[9] = {
-	{ 0, { -1, -2, +3, +4 } },
-	{ 1, { -1, -2, +3, +4 } },
-	{ 2, { -1, -4, +3, +4 } },
-	{ 3, { -1, -4, +3, +2 } },
-	{ 4, { -3, -1, +2, +4 } },
-	{ 5, { -3, -2, +1, +2 } },
-	{ 6, { -2, -3, +1, +4 } },
-	{ 7, { -1, -3, +4, +2 } },
-	{ 8, { -3, -4, +1, +2 } },
+	{ { -1, -2, +3, +4 } },
+	{ { -1, -2, +3, +4 } },
+	{ { -1, -4, +3, +4 } },
+	{ { -1, -4, +3, +2 } },
+	{ { -3, -1, +2, +4 } },
+	{ { -3, -2, +1, +2 } },
+	{ { -2, -3, +1, +4 } },
+	{ { -1, -3, +4, +2 } },
+	{ { -3, -4, +1, +2 } },
 };
 
-static CARD c[9];
-static int  r[9];
+static struct {
+	int no, rot;
+} field[9];
 static int used[9];
 
 static int debug = 0;
@@ -53,9 +55,9 @@ static void place_card(int idx)
 			continue;
 		used[no] = 1;
 
-		c[idx] = card[no];
-		for (rot = 0; rot < 4; rot++, rotate(idx)) {
-			r[idx] = rot;
+		for (rot = 0; rot < 4; rot++) {
+			field[idx].no  = no;
+			field[idx].rot = rot;
 
 			/* Check whether this card matches. */
 			if (!check(idx)) {
@@ -74,34 +76,31 @@ static void place_card(int idx)
 	}
 }
 
-static void rotate(int idx)
-{
-	int tmp;
-
-	tmp = c[idx].mark[0];
-	c[idx].mark[0] = c[idx].mark[1];
-	c[idx].mark[1] = c[idx].mark[2];
-	c[idx].mark[2] = c[idx].mark[3];
-	c[idx].mark[3] = tmp;
-}
-
 static int check(int idx)
 {
 	/* Check for match with the upper card. */
 	if (idx >= 3) {
 		int above = idx - 3;
-		if (c[idx].mark[1] + c[above].mark[3] != 0)
+		if (mark(idx, UP) + mark(above, DOWN) != 0)
 			return 0;
 	}
 
 	/* Check for match with the left card. */
 	if (idx % 3 != 0) {
 		int left = idx - 1;
-		if (c[idx].mark[2] + c[left].mark[0] != 0)
+		if (mark(idx, LEFT) + mark(left, RIGHT) != 0)
 			return 0;
 	}
 
 	return 1;
+}
+
+static int mark(int idx, int dir)
+{
+	int no  = field[idx].no;
+	int rot = field[idx].rot;
+
+	return card[no].mark[(rot + dir) % 4];
 }
 
 static void print(int idx, int brk_flag)
@@ -109,7 +108,7 @@ static void print(int idx, int brk_flag)
 	int k;
 
 	for (k = 0; k <= idx; k++)
-		printf("(%d,%d) ", c[k].no, r[k]);
+		printf("(%d,%d) ", field[k].no, field[k].rot);
 	if (brk_flag)
 		puts(": break");
 	else
